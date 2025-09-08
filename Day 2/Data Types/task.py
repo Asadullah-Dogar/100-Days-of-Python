9f59368a772618e197d561b677d8bddd5e24c2e5
Asadullah-Dogar <dogarasad277@gmail.com>
2025-09-09 00:38:14 +0500

    Added Day 2: Tip Calculator Project

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,21 @@
+#Subscripting
+print("Hello"[4])
+
+#string
+print('23' + '32') #concatenation
+
+# Integer = Whole number
+print(123 + 232)
+
+#large integer
+
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.1413444)
+
+# Boolean
+print(True)
+print(False)
+
+
---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,12 @@
+len('12345')
+
+print(type('hello'))
+print(type(1234))
+print(type(3.141))
+print(type(True))
+
+#type conversion or type casting
+
+print(int('1234') + int('324'))
+
+print("Number of letters in your name: ", len(input("Enter your name")))
